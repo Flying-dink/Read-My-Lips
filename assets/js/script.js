203b68fa27fe686b5240b3eb6c34622e493af296
Tony Rivera <ajboricua@gmail.com>
2021-07-29 18:26:35 -0400

    the real commit code wth css and timer fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var startGame = function() {
     score = 0 ;
     console.log(score);
     //reset time left
-    timeleft = 10;
+    timeleft = 90;
     //changes page to show choose elements for choose path and hide all others
     $('.cardContainer').removeClass('hide')
     $('#instructions').addClass('hide')

8881a585c0e9b0d83eb7f9d570de6003b9c13f9a
NukaGrizz <nukagrizz@gmail.com>
2021-07-29 10:21:17 -0400

    linked timeCountdown functionality with paths to end game when times up and show timesUp page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 //Get Elements by ID here (!!!DO NOT USE ANYTHING BUT GET BY UNIQUE ID NO CLASS SELECTOR NO ELEMENT SELECTORS NO QUERY SELECTORS!!!)
+var timerEl = document.getElementById('timerEl');
 
 //Global vars here
-var score = "";
-var timeleft = 90;
+var score = 0;
+var timeleft = 10;
 
 //function on start game clicked
 var startGame = function() {
     console.log('shit');
     //reset score to zero
     score = 0 ;
+    console.log(score);
     //reset time left
-    timeleft = 90;
+    timeleft = 10;
     //changes page to show choose elements for choose path and hide all others
     $('.cardContainer').removeClass('hide')
     $('#instructions').addClass('hide')
@@ -23,8 +25,6 @@ var startGame = function() {
 //timer function coundown to call end game once timer reaches zero and send user to score save
 var timerCountdown = function() {
      
-        
-     
         //use the 'setInterval()' to call a function to be executed every 1000 milliseconds
         var timeInterval = setInterval(function() {
      
@@ -32,13 +32,13 @@ var timerCountdown = function() {
             //As long as the 'timeleft' is greater than 1
             if (timeleft > 1) {
                 //Set the 'textContent' of the 'timerEl' to show the remaining seconds
-                timerEl.textContent = timeleft + 'seconds remaining';
+                timerEl.textContent = timeleft + ' seconds remaining';
                 //Decrement 'timeleft' by 1
                 timeleft--;
      
             }else if (timeleft ===1) {
                 //When ttimeleftime left is equal to 1, rename to 'second' instead of seconds
-                timerEl.textContent = timeleft + 'second remaining';
+                timerEl.textContent = timeleft + ' second remaining';
                 timeleft--;
             } else {
                 //Once 'timeLeft' gets to 0, set 'timerEl' to an empty string
@@ -46,7 +46,7 @@ var timerCountdown = function() {
                 //Use 'clearInterval()' to stop the timer
                 clearInterval(timeInterval);
                 //Call the 'displayMessage() function
-                displayMessage();
+                displayScore();
             }
      
         }, 1000);
@@ -54,16 +54,18 @@ var timerCountdown = function() {
     //upon reaching zero go to submitscore function;
     //function to start facebook path that changes page to show facebook posts to be read
 var JokePath = function() {
-    console.log('test');
+    
     //game elements show/other elements hide
     $('#jokeParentEl').addClass('hide')
     $('#gamePageParEl').removeClass('hide')
+    getJokeApi();
+    timerCountdown();
+}
 
-    //fetch facebook post loop parse and display per each question
-    var getJokeApi = function() {
-    fetch(
-        'https://official-joke-api.appspot.com/random_joke'
-      )
+//fetch facebook post loop parse and display per each question
+var getJokeApi = function() {
+    fetch('https://official-joke-api.appspot.com/random_joke')
+      
         // Converts the response to JSON
         .then(function(response) {
           return response.json();
@@ -78,18 +80,15 @@ var JokePath = function() {
 
         });
         document.getElementById('correctbutton').addEventListener('click', function(){
-            score++
+            score++;
+            console.log(score);
             getJokeApi();
         }); 
         document.getElementById('wrongbutton').addEventListener('click', function(){
             getJokeApi();
         });
-
-    }
-    getJokeApi();
-
-}   
-
+}    
+    
 
 var giphyPath = function() {
     console.log('test')
@@ -97,54 +96,47 @@ var giphyPath = function() {
     $('#gamePageParEl').removeClass('hide')
     $('#answer').addClass('hide')
     $('#giphyPic').removeClass('hide')
-    
+    getGiphyApi();
+    timerCountdown();
+};
 
-    var getGiphyApi = function() {
-        console.log('joe');
-        fetch('https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN')
-        // Converts the response to JSON
-    
-        .then(function(response) {
-        return response.json();
-        })
-        .then(function(response) {
-    
-        var myImage = document.getElementById('giphyPic');
-        console.log(response);
-            console.log(myImage.src)
-            console.log(response.data.image_url)
-            $('#giphyPic').attr("src",response.data.image_url);
+var getGiphyApi = function() {
+    console.log(score);
+    fetch('https://api.giphy.com/v1/gifs/random?api_key=HvaacROi9w5oQCDYHSIk42eiDSIXH3FN')
+    // Converts the response to JSON
 
-          
-            
-        document.getElementById('question').textContent = "Describe the picture!"
-        })};
-    
-        document.getElementById('correctbutton').addEventListener('click', function(){
-        score++
-            getGiphyApi();
-        });
-    
-        document.getElementById('wrongbutton').addEventListener('click', function(){
-            getGiphyApi();
-        });
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(response) {
 
+    var myImage = document.getElementById('giphyPic');
+    console.log(response);
+        console.log(myImage.src)
+        console.log(response.data.image_url)
+        $('#giphyPic').attr("src",response.data.image_url);
         
-
-        getGiphyApi();
+    document.getElementById('question').textContent = "Describe the picture!"
+    })
 };
-    
-    //local function for correct clicks log correct clicks under score change post displayed
-    //local function for correct or skip clicks change post displayed
 
-    
-//function to start twitter path
-var twitterPath = function() {
-    //game elements show/other elements hide
-    //fetch facebook post loop parse and display per each question
-    //log correct clicks under score
-    //correct or skip clicks change post displayed
+document.getElementById('correctbutton').addEventListener('click', function(){
+    score++;
+    getGiphyApi();
+});
+
+document.getElementById('wrongbutton').addEventListener('click', function(){
+    getGiphyApi();
+});    
+
+
+var displayScore = function() {
+    $('#gamePageParEl').addClass('hide')
+    $('#timesUpEls').removeClass('hide')  
 };
+
+// create object ot of new score to be saved in saveScores function
+ 
 
 //function to input your name and submit highscore
 var submitScore = function() {
@@ -155,7 +147,11 @@ var submitScore = function() {
     //if array not in localstorage then add new array object
 };
 
-var displayScore = function() {
+
+
+
+
+var displayHighScore = function() {
     //highscore page elements show/other elements hide
     //pull scores from localstorage
     //order scores in highest to lowest
